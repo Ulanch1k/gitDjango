f62629e857094bfb799c4406d95d68bb2989244b
Ulanch1k <dyusembaev.ulan@gmail.com>
2021-11-15 20:45:14 +0600

    create_film fix

--- django/onlinekino/movies/models.py
+++ django/onlinekino/movies/models.py
@@ -7,16 +7,15 @@ class Genre(models.Model):
     def __str__(self):
         return self.name
 
+class Director(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField(null=True, blank=True)
 
 class Movie(models.Model):
     title = models.CharField(max_length=40)
     description = models.TextField(null=True)
     rating = models.FloatField()
     release_date = models.DateField(auto_now=True)
-    genre = models.ManyToManyField(Genre)
-
-class Director(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    date_of_birth = models.DateField(null=True, blank=True)
-    date_of_death = models.DateField(null=True, blank=True)
+    genre = models.ManyToManyField(Genre, default='Фильм')
